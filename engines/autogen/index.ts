4246aa1a63009a16521010068ccd7ecbd1012cbe
Craig Huckerby <craighckby@gmail.com>
2026-09-24 21:58:34 +1000

    feat(autogen): add index.ts module export barrel (auto-push)

---
+++ engines/autogen/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ * Engine Package Exports for autogen
+ */
+
+export * from './01-agentruntimeengine';
+export * from './02-orchestrationworkflowengine';
+export * from './03-stateserializationengine';
+export * from './runtime';
