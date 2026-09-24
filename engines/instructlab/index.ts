e4324a8220fc8b0a0f07ed68a3e3c19415e5029b
Craig Huckerby <craighckby@gmail.com>
2026-09-24 22:00:47 +1000

    feat(instructlab): add index.ts module export barrel (auto-push)

---
+++ engines/instructlab/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ * Engine Package Exports for instructlab
+ */
+
+export * from './01-taxonomy-data-processor';
+export * from './02-synthetic-data-orchestrator';
+export * from './03-model-alignment-controller';
+export * from './runtime';
