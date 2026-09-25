b88b1a8aa61cc2c40a3952f25a43510142a36ebf
Craig Huckerby <craighckby@gmail.com>
2026-09-26 00:16:09 +1000

    feat(ppt-master): add index.ts module export barrel (auto-push)

---
+++ engines/ppt-master/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * SPDX-License-Identifier: MIT
+ * Engine Package Exports for ppt-master
+ * Source Origin: hugohe3/ppt-master
+ */
+
+export * from './01-ppt-master-lifecycle-kernel';
+export * from './02-ppt-master-react-loop-engine';
+export * from './03-ppt-master-unified-model-stream-adapter';
+export * from './04-ppt-master-tool-sandbox-virtual-file-system-engine';
+export * from './05-ppt-master-non-linear-session-tree-token-budget-engine';
+export * from './runtime';
